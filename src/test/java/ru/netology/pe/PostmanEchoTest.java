472c42d31ce0e1022ab5b73d323a568830e5195b
Aleksandr <tsetserev123@gmail.com>
2022-08-21 14:03:57 +0300

    исправление коммит

--- src/test/java/ru/netology/pe/PostmanEchoTest.java
+++ src/test/java/ru/netology/pe/PostmanEchoTest.java
@@ -17,7 +17,7 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("HI1"))
+                .body("data", equalTo("HI"))
         ;
     }
 }
